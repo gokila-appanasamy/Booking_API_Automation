80839d1f7451e95dff12b755d09c557a599b97d9
gokila-appanasamy <gokila.srm@gmail.com>
2025-02-27 17:53:02 +0530

    Commiting framework setup and automation script for Authentication login feature file, step definition, utility functions and properties file.

--- src/test/java/combooking/features/booking_CRUD.feature
+++ src/test/java/combooking/features/booking_CRUD.feature
@@ -1,8 +1,20 @@
 @bookingE2ETest
 
 Feature: End to End Booking Tests
-  Scenario: To perform a CRUD operation on booking API
+  Background: To perform a CRUD operation on booking API
 
-    Given user has access to endpoint "/auth/login"
+    Given user accesses endpoint "/auth/login"
     When user creates a auth token with login authentication as "admin" and "password"
     Then user should get the response code 200
+
+  Scenario Outline: To perform a CRUD operation on booking API
+
+    Given user accesses endpoint "/booking/"
+    When the user books a room with the mentioned booking details
+      | firstname   | lastname   | email   | phone   | checkin   | checkout   |
+      | <firstname> | <lastname> | <email> | <phone> | <checkin> | <checkout> |
+    Then the response status code should be 201
+
+    Examples:
+      | firstname | lastname | email              | phone         | checkin    | checkout   |
+      | Goki      | App      | goki.app@gmail.com | 1234567890098 | 2025-05-12 | 2025-05-14 |
--- src/test/java/combooking/step_definitions/Booking_CRUD.java
+++ src/test/java/combooking/step_definitions/Booking_CRUD.java
@@ -11,14 +11,19 @@ import org.json.JSONObject;
 import static org.junit.Assert.assertEquals;
 
 import io.restassured.response.Response;
+import org.junit.Assert;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 public class Booking_CRUD {
     Response response;
     JSONObject requestBody;
     BookingUtility bookingUtility;
 
-    @Given("user has access to endpoint {string}")
-    public void user_has_access_to_endpoint(String endpoint) {
+    @Given("user accesses endpoint {string}")
+    public void user_accesses_endpoint(String endpoint) {
         bookingUtility = new BookingUtility();
         bookingUtility.setEndPoint(endpoint);
     }
@@ -29,7 +34,7 @@ public class Booking_CRUD {
         loginAuth.put("username", userName);
         loginAuth.put("password", password);
         bookingUtility.response =
-                        bookingUtility.
+                bookingUtility.
                         requestSetup().
                         body(loginAuth.toString())
                         .when().
@@ -47,4 +52,64 @@ public class Booking_CRUD {
         assertEquals(Long.valueOf(statusCode), Long.valueOf(bookingUtility.response.getStatusCode()));
     }
 
+    @When("the user books a room with the mentioned booking details")
+    public void the_user_books_a_room_with_the_mentioned_booking_details(io.cucumber.datatable.DataTable dataTable) {
+        List<Map<String, String>> data = dataTable.asMaps(String.class, String.class);
+        for (Map<String, String> row : data) {
+            int roomid = Integer.parseInt(generateRandomRoomId());
+            bookingUtility.setRoomId(roomid);
+            requestBody = createBookingRequestBody(row, roomid);
+            response = bookingUtility.requestSetup().body(requestBody.toString()).when().post(bookingUtility.getEndPoint());
+            validateBookingResponse(row.get("firstname"), row.get("lastname"), row.get("checkin"), row.get("checkout"));
+
+            if (response.getStatusCode() == 201) {
+                int bookingId = response.jsonPath().getInt("booking.bookingid");
+                bookingUtility.setBookingId(bookingId);
+            }
+        }
+    }
+
+    @Then("the response status code should be {int}")
+    public void the_response_status_code_should_be(int expectedStatusCode) {
+        assertEquals(expectedStatusCode, response.getStatusCode());
+    }
+
+    private static String generateRandomRoomId() {
+
+        final Random random = new Random();
+        return String.valueOf(2000 + random.nextInt(900));
+
+    }
+
+    private JSONObject createBookingRequestBody(Map<String, String> row, int roomid) {
+
+        return new JSONObject()
+                .put("bookingid", 0)
+                .put("roomid", roomid)
+                .put("firstname", row.get("firstname"))
+                .put("lastname", row.get("lastname"))
+                .put("depositpaid", true)
+                .put("email", row.get("email"))
+                .put("phone", row.get("phone"))
+                .put("bookingdates", new JSONObject()
+                        .put("checkin", row.get("checkin"))
+                        .put("checkout", row.get("checkout")));
+
+    }
+
+    private void validateBookingResponse(String firstname, String lastname, String checkin, String checkout) {
+
+        Integer bookingId = response.jsonPath().getInt("bookingid");
+        String responseFirstname = response.jsonPath().getString("booking.firstname");
+        String responseLastname = response.jsonPath().getString("booking.lastname");
+        String responseCheckin = response.jsonPath().getString("booking.bookingdates.checkin");
+        String responseCheckout = response.jsonPath().getString("booking.bookingdates.checkout");
+
+        Assert.assertNotNull("BookingID should not to be null",bookingId);
+        assertEquals("First Name did not match", firstname, responseFirstname);
+        assertEquals("Last Name did not match", lastname, responseLastname);
+        assertEquals("Check in date did not match", checkin, responseCheckin);
+        assertEquals("Checkout date did not match", checkout, responseCheckout);
+
+    }
 }
